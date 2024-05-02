3ea96a702b0011fbc08853c3b585f346e09b212f
paviag <98894987+paviag@users.noreply.github.com>
2024-05-02 18:08:28 -0500

    Update UserBookSales.js

--- screens/UserBookSales.js
+++ screens/UserBookSales.js
@@ -10,15 +10,21 @@ import BookSale from '../components/BookSale';
 import TopBar from '../components/TopBar';
 import BottomNavBar from '../components/BottomNavBar';
 
-export default function UserBookSales() {
+export default function UserBookSales({route, navigation}) {
   return (
     <View style={styles.view0}>
       <SafeAreaView style={{width: '100%', flex: 1}}>
 
         <TopBar user="TomCherry" />
         
-        <ScrollView style={{alignItems: 'center'}}>
-          <View style={{alignItems: 'center', gap: 30, width: '90%'}}>
+        <ScrollView style={{alignItems: 'center', marginBottom: 40}}>
+          <View style={{alignItems: 'center', gap: 20, width: '90%'}}>
+            <PurpleButton 
+              title="Agregar" 
+              style={{marginTop: 10}} 
+              onPress={() => navigation.navigate('Crear venta nueva')}
+            />
+
             <BookSale 
               id="" 
               title="La Cadena de Hierro" 
@@ -30,13 +36,24 @@ export default function UserBookSales() {
               status="VENTA ACTIVA" 
               date="24/12/2025"
             />
-            <PurpleButton title="Agregar" style={{marginBottom: 40}}/>
+
+            <BookSale 
+              id="" 
+              title="La Cadena de Hierro" 
+              author="Cassandra Clare" 
+              editorial="Destino" 
+              year="2024" 
+              cover="Blanda" 
+              price={20000} 
+              status="VENTA ACTIVA" 
+              date="24/12/2025"
+            />
+
           </View>
         </ScrollView>
-
-        <BottomNavBar ventasActive={true} />
         
       </SafeAreaView>
+      <BottomNavBar ventasActive={true} />
     </View>
   );
 }
