9846e7132e360deddefae0476173369cb7723bb5
EdgarGXI <98917634+EdgarGXI@users.noreply.github.com>
2024-05-11 14:48:42 -0500

    Add files via upload

--- screens/CreateBookSale.js
+++ screens/CreateBookSale.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { View, StyleSheet, ScrollView, SafeAreaView, Image } from "react-native";
-//import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
 import * as ImagePicker from 'expo-image-picker';
 import SectionedMultiSelect from 'react-native-sectioned-multi-select';
 import { MaterialIcons as Icon } from '@expo/vector-icons';
@@ -9,14 +8,13 @@ import { PurpleButton, WhiteButton } from '../components/Buttons';
 import SimpleInput from '../components/SimpleInput';
 import TopBar from '../components/TopBar';
 import { NormalText } from '../components/FontSizing';
-
+import { insertRow } from '../db'; // Asumiendo que tienes una función importada para insertar filas en la base de datos
 
 export default function CreateBookSale() {
   const [image, setImage] = useState(null);
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      //allowsEditing: true,
       aspect: [6, 10],
     });
 
@@ -42,19 +40,69 @@ export default function CreateBookSale() {
     {name: "flojera", id: 5},
   ]
 
+  const [data, setFormData] = useState({
+    'title': '',
+    'author': '',
+    'editorial': '',
+    'year': '',
+    'price': '',
+    'cover': '',
+    'description': '',
+    'marked': false,
+    'damaged': false,
+    'status': 'available',
+    'buyer': null,
+    'date': new Date().toISOString(),
+  });
+
+    
+
+  const handlePublish = async () => {
+    try {
+      await insertRow({
+        table: 'booksale',
+        row: {
+          //sellerid: 'userId', // Reemplaza 'userId' con el ID del usuario actual
+          title: data["title"],
+          author: data["author"],
+          editorial: data["editorial"],
+          year: data["year"],
+          //cover: image.uri, // Asumiendo que image.uri contiene la ruta de la imagen seleccionada
+          //price: data["price"],
+          //description: formData.description,
+          //marked: formData.marked,
+          //damaged: formData.damaged,
+          //status: formData.status,
+          //buyer: formData.buyer,
+          //date: formData.date,
+        },
+      });
+      // Aquí puedes realizar alguna acción adicional después de insertar la fila, como navegar a otra pantalla
+    } catch (error) {
+      console.error('Error al insertar la fila:', error);
+    }
+  };
+
+  const handleChange = (id, text) => {
+    setFormData(data => ({
+      ...data,
+      [id]: text,
+    }));
+  };
+
   return (
     <View style={styles.view0}>
       <SafeAreaView style={{width: '100%', flex: 1}}>
         
         <ScrollView>
           <View style={{alignItems: 'center'}}>
-            <SimpleInput placeholder="Nombre del libro" fontSize={18} simple={true} styleDiv={{width: '80%', justifyContent: 'center'}} styleInput={{textAlign: 'center'}}/>
-            <SimpleInput placeholder="Autor" simple={true} styleDiv={{width: '80%', justifyContent: 'center'}} styleInput={{textAlign: 'center'}}/>
+            <SimpleInput placeholder="Nombre del libro" fontSize={18} simple={true} styleDiv={{width: '80%', justifyContent: 'center'}} styleInput={{textAlign: 'center'}} defaultValue={data["title"]} onChangeText={newText => handleChange("title", newText)}/>
+            <SimpleInput placeholder="Autor" simple={true} styleDiv={{width: '80%', justifyContent: 'center'}} styleInput={{textAlign: 'center'}} defaultValue={data["author"]} onChangeText={newText => handleChange("author", newText)}/>
             <View style={{flexDirection: 'row', width:'80%', justifyContent: 'space-between'}}>
-              <SimpleInput placeholder="Editorial" simple={true} styleDiv={{width: '50%', justifyContent: 'center'}} styleInput={{textAlign: 'center'}}/>
-              <SimpleInput placeholder="Año" simple={true} inputMode="numeric" maxLength={4} styleDiv={{width: '50%', justifyContent: 'center'}} styleInput={{textAlign: 'center'}}/>
+              <SimpleInput placeholder="Editorial" simple={true} styleDiv={{width: '50%', justifyContent: 'center'}} styleInput={{textAlign: 'center'}} defaultValue={data["editorial"]} onChangeText={newText => handleChange("editorial", newText)}/>
+              <SimpleInput placeholder="Año" simple={true} inputMode="numeric" maxLength={4} styleDiv={{width: '50%', justifyContent: 'center'}} styleInput={{textAlign: 'center'}} defaultValue={data["year"]} onChangeText={newText => handleChange("year", newText)}/>
             </View>
-            <SimpleInput placeholder="Precio" simple={true} inputMode="decimal" icon={<NormalText>$</NormalText>} styleDiv={{width: '80%', justifyContent: 'center'}} />
+            <SimpleInput placeholder="Precio" simple={true} inputMode="decimal" icon={<NormalText>$</NormalText>} styleDiv={{width: '80%', justifyContent: 'center'}} defaultValue={data["price"]} onChangeText={newText => handleChange("price", newText)}/>
             <SimpleInput placeholder="Tipo de tapa (Blanda/Dura)" simple={true} styleDiv={{width: '80%', justifyContent: 'center'}} />
             <View style={{width: 210, backgroundColor: '#ccc', marginVertical: 20, height: 300}}>
               <Image 
@@ -108,6 +156,7 @@ export default function CreateBookSale() {
             <PurpleButton 
               title="Publicar"
               style={{paddingHorizontal: 40, marginTop: 10, marginBottom: 40}} 
+              onPress={handlePublish}
             />
           </View>
         </ScrollView>
@@ -124,7 +173,7 @@ const styles = StyleSheet.create({
   },
   multiSelectBackdrop: {
     backgroundColor: 'rgba(204,204,204,0.6)',
-    borderRadius: '30%',
+    borderRadius: 30,
   },
   multiSelectBox: {
     borderWidth: 1,
