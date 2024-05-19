7250e13e9fc110bbb1f8660296297e5bef020301
paviag <98894987+paviag@users.noreply.github.com>
2024-05-19 12:17:12 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,83 +1,51 @@
+import React from 'react';
 import { supabase } from './supabase.ts';
 
 export async function fetchAll(table) {
-  try {
-    const { data, error } = await supabase.from(table).select();
-    if (error) throw error;
-    return data;
-  } catch (error) {
-    throw error;
-  }
+  const { data, error } = await supabase.from(table).select();
+  if (error) throw error;
+  return data;
 }
 
 export async function deleteRow(table, column, value) {
-  try {
-    const { error } = await supabase
-      .from(table)
-      .delete()
-      .eq(column, value);
-    if (error) throw error;
-  } catch (error) {
-    console.log('Error', error)
-    throw error;
-  }
+  const { error } = await supabase
+    .from(table)
+    .delete()
+    .eq(column, value);
+  if (error) throw error;
 }
 
 export async function insertRow(table, row) {
-  try {
-    const { status, error } = await supabase.from(table).insert(row);
-    if (String(status)[0] !== '2') throw error;
-  } catch (error) {
-    console.log('Error', error);
-    throw error;
-  }
+  const { status, error } = await supabase.from(table).insert(row);
+  if (String(status)[0] !== '2') throw error;
 }
 
 export async function insertRowReturn(table, row) {
-  try {
-    const { data, status, error } = await supabase.from(table).insert(row).select();
-    if (String(status)[0] !== '2') throw error;
-    return data[0];
-  } catch (error) {
-    console.log('Error', error);
-    throw error;
-  }
+  const { data, status, error } = await supabase.from(table).insert(row).select();
+  if (String(status)[0] !== '2') throw error;
+  return data[0];
 }
 
 export async function fetchByColumn(table, column, value, select=null) {
-  try {
-    const { data, error } = await supabase.from(table).select(select).eq(column, value);
-    if (error) throw error;
-    return data;
-  } catch (error) {
-    console.log('Error', error)
-    return null;
-  }
+  const { data, error } = await supabase.from(table).select(select).eq(column, value);
+  if (error) return null;
+  return data;
 }
 
 export async function upsertRow(table, data) {
-  try {
-    const { status, error } = await supabase
-      .from(table)
-      .upsert(data);
-    if (String(status)[0] !== '2') throw error;
-  } catch (error) {
-    console.log('Error', error);
-    throw error;
-  }
+  const { status, error } = await supabase
+    .from(table)
+    .upsert(data);
+  if (String(status)[0] !== '2') throw error;
 }
 
 export async function getCountConditional(table, conditionColumn, conditionValue) {
-  try {
-    const { count, error } = await supabase
-      .from(table)
-      .select('*', { count: 'exact', head: true })
-      .eq(conditionColumn, conditionValue);
-    if (error) throw error;
-    return count;
-  } catch (error) {
-    return 0;
-  }
+  const { count, error } = await supabase
+    .from(table)
+    .select('*', { count: 'exact', head: true })
+    .eq(conditionColumn, conditionValue);
+  if (error) return 0;
+  return count;
 }
 
 export async function getSellerStats(userid) {
@@ -87,6 +55,7 @@ export async function getSellerStats(userid) {
       .select('price')
       .eq('sellerid', userid)
       .eq('status', 'FINALIZADA');
+    if (error) throw error;
     let count = await getCountConditional('booksale', 'sellerid', userid);
     return ({
       published: count,
@@ -103,11 +72,36 @@ export async function getSellerStats(userid) {
 }
 
 export async function fetchBookOffersByUser(userid) {
-  try {
-    const { data, error } = await supabase
-      .from('bookoffer')
-      .select(`
-        booksaleid,
+  const { data, error } = await supabase
+    .from('bookoffer')
+    .select(`
+      booksaleid,
+      bookofferid,
+      deliveryaddress,
+      date,
+      status,
+      account(
+        name,
+        accountid
+      ),
+      booksale(
+        title,
+        author
+      )
+    `)
+    .eq('accountid', userid);
+  if (error) return [];
+  return data;
+}
+
+export async function fetchBookOffersForUser(userid) {
+  const { data, error } = await supabase
+    .from('booksale')
+    .select(`
+      booksaleid,
+      title,
+      author,
+      bookoffer!inner(
         bookofferid,
         deliveryaddress,
         date,
@@ -115,47 +109,12 @@ export async function fetchBookOffersByUser(userid) {
         account(
           name,
           accountid
-        ),
-        booksale(
-          title,
-          author
         )
-      `)
-      .eq('accountid', userid);
-    if (error) throw error;
-    return data;
-  } catch (error) {
-    console.log(error)
-    return [];
-  }
-}
-
-export async function fetchBookOffersForUser(userid) {
-  try {
-    const { data, error } = await supabase
-      .from('booksale')
-      .select(`
-        booksaleid,
-        title,
-        author,
-        bookoffer!inner(
-          bookofferid,
-          deliveryaddress,
-          date,
-          status,
-          account(
-            name,
-            accountid
-          )
-        )
-      `)
-      .eq('sellerid', userid);
-    if (error) throw error;
-    return data;
-  } catch (error) {
-    console.log('Error', error)
-    return [];
-  }
+      )
+    `)
+    .eq('sellerid', userid);
+  if (error) return [];
+  return data;
 }
 
 export async function fetchBookSalesByUser(userid) {
@@ -167,34 +126,30 @@ export async function fetchBookSalesByUser(userid) {
 }
 
 export async function fetchBookSaleDetails(booksaleid) {
-  try {
-    const { data, error } = await supabase
-      .from('booksale')
-      .select(`
-        title, 
-        author, 
-        editorial, 
-        year, 
-        cover, 
-        price, 
-        marked, 
-        damaged, 
-        description,
-        status,
-        account:sellerid (
-          name,
-          accountid
-        ),
-        bookgenre(
-          genre
-        )
-      `)
-      .eq('booksaleid', booksaleid)
-    if (error) throw error;
-    return data[0];
-  } catch (error) {
-    return null;
-  }
+  const { data, error } = await supabase
+    .from('booksale')
+    .select(`
+      title, 
+      author, 
+      editorial, 
+      year, 
+      cover, 
+      price, 
+      marked, 
+      damaged, 
+      description,
+      status,
+      account:sellerid (
+        name,
+        accountid
+      ),
+      bookgenre(
+        genre
+      )
+    `)
+    .eq('booksaleid', booksaleid)
+  if (error) return null;
+  return data[0];
 }
 
 export async function fetchUserData(userid, select=null) {
@@ -207,28 +162,49 @@ export async function fetchUserData(userid, select=null) {
 }
 
 export async function uncheckedOffers(userid) {
-  try {
-    const { data, error } = await supabase
-      .from('booksale')
-      .select('bookoffer!inner(status)')
-      .eq('sellerid', userid)
-      .eq('bookoffer.status', 'PENDIENTE');
-    if (error) throw error;
-    return data.length;
-  } catch (error) {
-    return 0;
-  }
+  const { data, error } = await supabase
+    .from('booksale')
+    .select('bookoffer!inner(status)')
+    .eq('sellerid', userid)
+    .eq('bookoffer.status', 'PENDIENTE');
+  if (error) return 0;
+  return data.length;
 }
 
 export async function changeOfferStatus(id, newStatus) {
-  try { 
-    const { error } = await supabase
-      .from('bookoffer')
-      .update({ status: newStatus })
-      .eq('bookofferid', id);
-    if (error) throw error;
-  } catch (error) {
-    console.log('Error', error)
-    throw error;
-  }
+  const { error } = await supabase
+    .from('bookoffer')
+    .update({ status: newStatus })
+    .eq('bookofferid', id);
+  if (error) throw error;
+}
+
+export async function updateImage(id, newimage) {
+  const { error } = await supabase
+    .from('booksale')
+    .update({ img: newimage })
+    .eq('booksaleid', id);
+  if (error) throw error;
+}
+
+export async function fetchImage(bucket, name) {
+  const { data } = await supabase.storage.from(bucket).getPublicUrl(name);
+  return data.publicUrl;
+}
+
+export async function uploadFile(bucket, path, file) {
+  const { data, error } = await supabase.storage.from(bucket).upload(path, file, {
+    //upsert: true,
+    contentType: 'image/jpeg',
+  });
+  if (error) {
+    console.log('error',error)
+  } 
+}
+
+export async function getAvatar(userid) {
+  let user = await fetchByColumn('account', 'accountid', userid);
+  user = user[0];
+  let imgLink = await fetchImage('avatars', user.email);
+  return imgLink;
 }
